f63c7d23b8e41c7475573b2bf8624cfe0f2177bd
Swagat <swagatswaroop@gmail.com>
2023-10-20 23:07:36 +0530

    Done

--- JavaScript/ExceptionHandling/trycatch.js
+++ JavaScript/ExceptionHandling/trycatch.js
@@ -1,1 +1,22 @@
 //Exception :- unexpected issue
+
+//Naive approach
+// let a = "hello";
+// if(a != undefined){
+//     throw new Error("Swagat");
+// }
+// else{
+//     console.log("this is undefined");
+// }
+
+
+
+//Try and catch block
+try{
+    let a = 5;
+    console.log("we are inside the try block");
+    blockchain();
+
+}catch(error){
+    console.log("Something went wrong");
+}
