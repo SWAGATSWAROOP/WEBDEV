a53e73b57b2a8a36da7186c9377f88574417112c
Swagat <swagatswaroop@gmail.com>
2023-10-25 20:34:30 +0530

    Using express writing code

--- Http Server/api.js
+++ Http Server/api.js
@@ -6,6 +6,11 @@ app.get('/',(req,res)=>{
     res.send("hello");
 });
 
+//Adding a new route
+app.get('/swagat',(req,res) =>{
+    res.send("Hello Swagat");
+})
+
 const port = 3000;
 app.listen(port,()=>console.log(`listeng on port ${port}`));
 
