b48ea5ecfbdb798c91acbc520113f7cc54ef045b
Swagat <swagatswaroop@gmail.com>
2023-10-25 20:24:04 +0530

    Making API full fledged

--- Http Server/api.js
+++ Http Server/api.js
@@ -1,3 +1,11 @@
 const express = require('express');
+
+//App is an object used to interact with express functional
 const app = express();
+app.get('/',(req,res)=>{
+    res.send("hello");
+});
+
+const port = 3000;
+app.listen(port,()=>console.log(`listeng on port ${port}`));
 
