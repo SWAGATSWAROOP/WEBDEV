e829850e02c5d17bcfc1740f97e95e2b30fb4b3f
Swagat <swagatswaroop@gmail.com>
2023-11-04 17:41:37 +0530

    React Practice

--- React/aryan/src/index.js
+++ React/aryan/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Swagat(){
+    return (
+      <div className='swagat'>
+        <BlockChain />
+        <Reactjs />
+        <h1>Swagat Swaroop Parida</h1>
+      </div>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BlockChain = ()=>{
+    return <h4>BlockChain + React = Deadly</h4>
+}
+
+const Reactjs = () =>{
+    return <h4>Reactjs is best UI Library.</h4>
+}
+
+ReactDOM.render( <Swagat />,document.getElementById('root'));
