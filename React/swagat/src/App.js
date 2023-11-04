19f0203c976ac43a437f8e7e4f2ffd5414152f4a
Swagat <swagatswaroop@gmail.com>
2023-11-04 15:28:09 +0530

    Practices

--- React/swagat/src/App.js
+++ React/swagat/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 //Whenever imorting directly from the Arrow Function
 import { Welcome } from './component/Swagat';
+import Swaroop from './component/Swaroop';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <Swaroop />
     </div>
   );
 }
---
+++ React/swagat/src/component/Swaroop.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react"; //Writing Only in Class Based
+
+class Swaroop extends Component{
+    render(){
+        return <h1>Class Based Component</h1>
+    }
+}
+
+export default Swaroop;
