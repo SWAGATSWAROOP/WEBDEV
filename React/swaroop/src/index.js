118735b0ea74773285516a0667a5883174ad2d9f
Swagat <swagatswaroop@gmail.com>
2023-11-04 17:59:20 +0530

    Done

--- React/swaroop/src/index.js
+++ React/swaroop/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Swagat from "./components/swagat";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Main(){
+  return(
+    <div>
+      <Swagat />
+      <h3>Aryan Swaroop Parida</h3>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Main />, document.getElementById('root'));
