b3989d3349b8af14e622971a1ea1aa7e2ea68990
Swagat <swagatswaroop@gmail.com>
2023-11-07 23:13:04 +0530

    Typos

--- React/books/src/index.js
+++ React/books/src/index.js
@@ -2,29 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Info = {
+const Info1 = {
   title: "Cryptocurrency: Bitcoin & Blockchain: 4 Books in 1 Cryptocurrency",
   author: "Swagat",
   image: "https://m.media-amazon.com/images/I/418118R+a9L._SY445_SX342_.jpg",
 };
 
+const Info2 = {
+  title:
+    "Coding for Kids: Python & Blockchain Programming : A Beginners Guide for Future App Developers - 100+ Activities (2 in 1 C...",
+  author: "Elliot Davis ",
+  image:
+    "https://m.media-amazon.com/images/I/81aStir06GL._AC_UY436_FMwebp_QL65_.jpg",
+};
+
+const Info3 = {
+  title: "Cryptocurrency",
+  author: "Aryan",
+  image:
+    "https://m.media-amazon.com/images/I/91eoDgHV6WL._AC_UY436_FMwebp_QL65_.jpg",
+};
+
+const Info4 = {
+  title: "AWS",
+  author: "Swagat",
+  image:
+    "https://m.media-amazon.com/images/I/61isV+qaN0L._AC_UY436_FMwebp_QL65_.jpg",
+};
+
 const Root = () => {
   return (
     <section className="bookList">
-      <Image title="Swagat"></Image>
-      <Image title="Munu" award="5 start excellence"></Image>
-      <Image title="Aryan"></Image>
-      <Image title="Litan Mamu"></Image>
+      <Image Object={Info1}></Image>
+      <Image Object={Info2} award="5 start excellence"></Image>
+      <Image Object={Info3}></Image>
+      <Image Object={Info4}></Image>
     </section>
   );
 };
 
 const Image = (props) => {
+  // Object destructuring
+  const { title, author, image } = props.Object;
   return (
     <div className="Books">
-      <h1>{Info.title}</h1>
-      <h5>{Info.author}</h5>
-      <img src={Info.image} alt="" />
+      <h1>{title}</h1>
+      <h5>{author}</h5>
+      <img src={image} alt="" />
       {/* Using Props */}
       <p>{props.title}</p>
       <h1>{props.award}</h1>
