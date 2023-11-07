6c2c3d71a39ba13eb7fc7a7852993da4490a91c3
Swagat <swagatswaroop@gmail.com>
2023-11-07 22:33:26 +0530

    Using Props

--- React/books/src/index.js
+++ React/books/src/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+
+const Info = {
+  title: "Cryptocurrency: Bitcoin & Blockchain: 4 Books in 1 Cryptocurrency",
+  author: "Swagat",
+  image: "https://m.media-amazon.com/images/I/418118R+a9L._SY445_SX342_.jpg",
+};
 
 const Root = () => {
   return (
-    <>
-      <Image />
-      <Author />
-      <Title />
-    </>
+    <section className="bookList">
+      <Image title="Swagat"></Image>
+      <Image title="Munu" award="5 start excellence"></Image>
+      <Image title="Aryan"></Image>
+      <Image title="Litan Mamu"></Image>
+    </section>
   );
 };
 
-const Image = () => {
+const Image = (props) => {
   return (
-    <img src="https://m.media-amazon.com/images/I/21Uxhb8lwYL.css?AUIClients/DetailPageABSMultiSelectionAssets&2VQcpB0s#desktop.542412-T1"></img>
+    <div className="Books">
+      <h1>{Info.title}</h1>
+      <h5>{Info.author}</h5>
+      <img src={Info.image} alt="" />
+      {/* Using Props */}
+      <p>{props.title}</p>
+      <h1>{props.award}</h1>
+    </div>
   );
-};
-
-const Title = () => {
-  return (
-    <h1>Cryptocurrency: Bitcoin & Blockchain: 4 Books in 1 Cryptocurrency</h1>
-  );
-};
-
-const Author = () => {
-  return <h2>Keizer Söze</h2>;
 };
 
 ReactDOM.render(<Root />, document.getElementById("root"));
